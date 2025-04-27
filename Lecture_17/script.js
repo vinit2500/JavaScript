78f70c09e8eed17b9b5dba5f1d5136722259d728
vinit solanki <vinitsolanki1245@gmail.com>
2025-04-27 22:41:42 +0530

    Starting javascript

---
+++ Lecture_17/script.js
@@ -0,0 +1,16 @@
+/*
+  For see the address of variable 
+  Go to memory tab -> in heap snapshot tick option -> click on take snapshort
+  then go to system context search for Akash
+  you will see "Akash"@24879
+  inside you will see sytem context @139777
+
+  @139777 search this you will find all variables in this context
+*/
+
+
+const firstName = "Akash"
+const userAge = 15
+const isGraduate = false
+
+
