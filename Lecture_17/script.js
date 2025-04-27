4381e2a33aed5f735a0c97c60c058c8a013bba05
vinit solanki <vinitsolanki1245@gmail.com>
2025-04-27 23:19:45 +0530

    completed lecture 17

--- Lecture_17/script.js
+++ Lecture_17/script.js
@@ -1,16 +1,27 @@
 /*
   For see the address of variable 
-  Go to memory tab -> in heap snapshot tick option -> click on take snapshort
+  Go to memory tab -> in heap snapshot tick option otherwise number address will not show
+  -> click on take snapshort
   then go to system context search for Akash
   you will see "Akash"@24879
   inside you will see sytem context @139777
 
-  @139777 search this you will find all variables in this context
+  @139777 search this you will find all variables in this context.
+
+  SABHI ka address odd number mai hota but do not know why?
 */
 
-
+// This all are primitive types s
 const firstName = "Akash"
+const firstName1 = "Akash" //address will be same for firstName and firstName1
+const firstName2 = "Ak" + "ash" // Now address will differ
+const lastName = "Singh"
 const userAge = 15
-const isGraduate = false
+const birthYear = 2002
+const isGraduate = false  //address is @75
+const isLogin = true //address is @73
+const myNull = null //address is @71
+const myUndefined = undefined //address is @67
+const myEmptyString = '' //address is @77
 
 
