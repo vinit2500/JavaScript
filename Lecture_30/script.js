003b8df92262ce6c9e895ea8aad05d02333d2317
vinit solanki <vinitsolanki1245@gmail.com>
2025-05-05 22:37:55 +0530

    completed call stack

---
+++ Lecture_30/script.js
@@ -0,0 +1,21 @@
+debugger
+
+sayHi()
+
+const username = "Vinit"
+const userage = 27
+
+function sayHi() {
+    const a = 14
+    const b = 12
+    add(2,3)
+}
+
+function add(x, y) {
+    return x + y
+}
+
+
+/*
+ we can only stop code execution phase not memory creation phase.
+*/
---
+++ Lecture_31/script.js
@@ -0,0 +1,32 @@
+debugger;
+
+// sayHi();
+
+// const username = "Vinit";
+// const userage = 27;
+
+// function sayHi() {
+//   const a = 14;
+//   const b = 12;
+//   add(2, 3);
+// }
+
+// function add(x, y) {
+//   check()
+//   return x + y;
+// }
+
+
+// function check() {
+//     console.log("This is check");
+// }
+
+function introduceMe() {
+    console.log("My name is anurag");
+    introduceMe()  //StackOverflow
+}
+
+introduceMe()
+
+console.log("Program Ended");
+
