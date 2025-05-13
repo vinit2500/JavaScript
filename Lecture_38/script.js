bb1fc3024160714e9302a71a00f80dc209a85c57
vinit solanki <vinitsolanki1245@gmail.com>
2025-05-13 21:38:44 +0530

    completed arrow function

--- Lecture_38/script.js
+++ Lecture_38/script.js
@@ -1,8 +1,23 @@
-const a = 4
-const b = 6
+// now parent has become higher order function.
+// HOF => ya to function ko as an argument le ya to return kre
+// clousure => jb bhi hum outer(parent) function se inner(child) function ko return krte hai aur us
+// outer function ke kch variables ko inner function mai use krte hai aur us outer function se
+// inner function ko return krwa dete hai to keval woh function return nhi hota uske jitne variable ko
+// apn ne use kiya hai woh sare variable bhi return ho jate hai
+//
 
-function add() {
-  console.log(a + b)
+function outer() {
+  const a = 4;
+  function parent() {
+    const b = 6;
+    function add() {
+      console.log(a + b);
+    }
+    return add;
+  }
+  return parent();
 }
 
-console.dir(add)
+const add1 = outer();
+// ye add1 ke sath scope bhi aata hai
+console.dir(add1);
---
+++ Lecture_40/script.js
@@ -0,0 +1,19 @@
+//Function decalaration
+// function square(num) {
+//    return num * num
+// }
+
+// Function expression
+// const square = function(num) {
+//        return num * num
+// }
+
+//Arrow function expression
+
+// const square = (num) => {
+//     return num * num
+// }
+
+const square = (num) => num * num; //implicit return 
+const square1 = num => num * num 
+
